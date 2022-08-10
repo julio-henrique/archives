89a8b94b9a507ae54bb9dc4a118dbff33f0313a9
Julio Henrique <juliohjesus@gmail.com>
2022-08-09 22:50:33 -0300

    add new challenge

--- challenges/script.js
+++ challenges/script.js
@@ -1,3 +1,9 @@
+const result = document.getElementById('result')
+
+
+
+// Running Sum Array
+// Some o número atual com o seu antecessor e retorne o array
 const runningSum = (nums) => {
     let newArr = []
     for (let i = 0; i < nums.length; i++) {
@@ -15,5 +21,21 @@ const runningSum = (nums) => {
         retornar o array com as somas dos itens
     */
 }
-
 runningSum([1, 1, 1, 1, 1])
+
+// ============================================================
+
+/**
+ * Primeiro caractere único em uma string
+ * Tendo uma string, encontre o primeiro caractere não repetido
+ * e retorne o seu index.
+ * 
+ * Se não existir, retorne -1.
+ * 
+ * Exemplo 
+ * str = 'javascript'
+ * retorna 0
+ * 
+ * str = 'ana'
+ * retorna 1
+*/
